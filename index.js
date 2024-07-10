b1d05965e9bd27e6716c901b1171cd80401e4d0a
+ <+>
2024-07-10 10:25:54 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  if(rideDistance <= 400) {
+    return 'This one is on me!';
+  }
+  else if(rideDistance > 2500) {
+    return 'No can do.';
+  }
+  else if(rideDistance > 400 && rideDistance <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if(rideDistance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(desinationCity){
+  if(desinationCity == 'NYC') {
+    return 'Ok, sounds good.';
+  }
+  else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+  if(Tip == 'generous') {
+    return 'Thank you so much.';
+  }
+  else if(Tip == 'not as generous') {
+    return 'Thank you.';
+  }
+  else {
+    return 'Bye.';
+  }
 }
